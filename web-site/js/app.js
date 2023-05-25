5a4f8b5cdb5ad3a03be146b4078c527de5522522
Roy Vanegas <roy@thecodeeducators.com>
2023-05-25 11:11:15 -0400

    Add app.js

---
+++ web-site/js/app.js
@@ -0,0 +1,3 @@
+// monkeytown--21-august-2008.jpg, Deidre Schoo, Performing at the now-defunct Monkeytown. Williamsburg, Brooklyn. 21 August 2008.
+// black-box-theatre--hunter-college--26-september-2009.jpg, Roy Vanegas, Live setup. Black Box Theatre, Hunter College. New York, NY. 26 September 2009.
+// home-studio--11-august-2007.jpg, Roy Vanegas, Home studio. Crown Heights, Brooklyn. 11 August 2007.

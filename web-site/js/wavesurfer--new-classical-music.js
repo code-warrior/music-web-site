b187dd48ddd32c6b1f8599005dd8426194a5b3dc
Roy Vanegas <roy@thecodeeducators.com>
2023-07-20 16:30:13 -0400

    Add the wind quintet miniature

--- web-site/js/wavesurfer--new-classical-music.js
+++ web-site/js/wavesurfer--new-classical-music.js
@@ -7,6 +7,8 @@ const canvas2D__TheDedication = document.createElement(`canvas`).getContext(`2d`
     gradient__TheDedication = canvas2D__TheDedication.createLinearGradient(0, 0, 0, 100),
     canvas2D__Tinistrind = document.createElement(`canvas`).getContext(`2d`),
     gradient__Tinistrind = canvas2D__Tinistrind.createLinearGradient(0, 0, 0, 100),
+    canvas2D__WindQuintetMiniature = document.createElement(`canvas`).getContext(`2d`),
+    gradient__WindQuintetMiniature = canvas2D__WindQuintetMiniature.createLinearGradient(0, 0, 0, 100),
     loaders = document.querySelectorAll(`.loader`);
 
 for (let i = 0; i < loaders.length; i++) {
@@ -19,8 +21,12 @@ gradient__TheDedication.addColorStop(.65, colors.WHITE);
 gradient__Tinistrind.addColorStop(0, colors.BLACK);
 gradient__Tinistrind.addColorStop(.65, colors.WHITE);
 
+gradient__WindQuintetMiniature.addColorStop(0, colors.BLACK);
+gradient__WindQuintetMiniature.addColorStop(.65, colors.WHITE);
+
 const PATH_TO_THE_DEDICATION = `media/audio/the-dedication.mp3`;
 const PATH_TO_TINISTRIND = `media/audio/tinistrind.mp3`;
+const PATH_TO_WIND_QUINTET_MINIATURE = `media/audio/wind-quintet-miniature.mp3`;
 
 const theDedicationWaveSurferPlayer = WaveSurfer.create({
     container: `#the-dedication-waveform`,
@@ -72,7 +78,33 @@ const tinistrindEnvelope = tinistrindWaveSurferPlayer.registerPlugin(
     })
 );
 
-const compositionTitles = [`the-dedication`, `tinistrind`];
+const windQuintetMiniatureWaveSurferPlayer = WaveSurfer.create({
+    container: `#wind-quintet-miniature-waveform`,
+    waveColor: gradient__TheDedication,
+    barWidth: consts.UNIVERSAL_BAR_WIDTH,
+    barGap: consts.UNIVERSAL_BAR_GAP,
+    autoplay: consts.UNIVERSAL_AUTO_PLAY,
+    cursorWidth: consts.UNIVERSAL_CURSOR_WIDTH,
+    height: consts.UNIVERSAL_WAVEFORM_HEIGHT,
+    barRadius: consts.UNIVERSAL_BAR_RADIUS,
+    progressColor: colors.GRAYISH,
+    url: PATH_TO_WIND_QUINTET_MINIATURE
+});
+
+const windQuintetMiniatureEnvelope = windQuintetMiniatureWaveSurferPlayer.registerPlugin(
+    EnvelopePlugin.create({
+        fadeInEnd: consts.UNIVERSAL_ENVELOPE_FADE_IN_END,
+        fadeOutStart: 68,
+        volume: consts.UNIVERSAL_ENVELOPE_INITIAL_VOLUME,
+        lineColor: consts.UNIVERSAL_ENVELOPE_LINE_COLOR,
+        lineWidth: consts.UNIVERSAL_ENVELOPE_LINE_WIDTH,
+        dragPointSize: consts.UNIVERSAL_ENVELOPE_DRAG_POINT_SIZE,
+        dragPointStroke: colors.BLACK
+    })
+);
+
+
+const compositionTitles = [`the-dedication`, `tinistrind`, `wind-quintet-miniature`];
 const waveformPlayer = document.getElementsByClassName(`composition__waveform`);
 const playButton = new Array(waveformPlayer.length);
 
@@ -122,6 +154,27 @@ tinistrindWaveSurferPlayer.on(`ready`, () => {
             tinistrindWaveSurferPlayer.pause();
         } else {
             tinistrindWaveSurferPlayer.play();
+        }
+    });
+});
+
+windQuintetMiniatureWaveSurferPlayer.on(`play`, () => {
+    playButton[2].textContent = `Pause`;
+});
+
+windQuintetMiniatureWaveSurferPlayer.on(`pause`, () => {
+    playButton[2].textContent = `Play`;
+});
+
+windQuintetMiniatureWaveSurferPlayer.on(`ready`, () => {
+    loaders[2].removeAttribute(`class`);
+    loaders[2].setAttribute(`class`, `disappear`);
+
+    playButton[2].addEventListener(`click`, () => {
+        if (windQuintetMiniatureWaveSurferPlayer.isPlaying()) {
+            windQuintetMiniatureWaveSurferPlayer.pause();
+        } else {
+            windQuintetMiniatureWaveSurferPlayer.play();
         }
     });
 });
